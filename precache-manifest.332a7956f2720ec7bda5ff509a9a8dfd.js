cef91c0567261851fd4db578980c01ed6e3c71b2
Fuad Sahmawi <35591248+fuadsahmawi@users.noreply.github.com>
2020-07-21 13:44:19 +0800

    Updates

---
+++ precache-manifest.332a7956f2720ec7bda5ff509a9a8dfd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "029dcff39dfe0b604aee1b4e3baffac4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c947df9ec1fa4c01af2",
+    "url": "/static/css/2.6f44476b.chunk.css"
+  },
+  {
+    "revision": "3c947df9ec1fa4c01af2",
+    "url": "/static/js/2.4df4d506.chunk.js"
+  },
+  {
+    "revision": "6fea5a45bbb56827f9f82c4c74c29929",
+    "url": "/static/js/2.4df4d506.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb7bfa7f9c9f2b196302",
+    "url": "/static/js/main.7f90ffc5.chunk.js"
+  },
+  {
+    "revision": "420d11f43645d04fc865",
+    "url": "/static/js/runtime-main.9b126251.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  }
+]);
